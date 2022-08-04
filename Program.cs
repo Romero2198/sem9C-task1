3b310217b36d5c2801cfba19488a21f73214f5c5
YungEnergy <40674522+Romero2198@users.noreply.github.com>
2022-08-04 19:14:26 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите число M: ");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите числоN : ");
+Console.Write("Введите число N : ");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(NaturalNumber(n, m));
 
