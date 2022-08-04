9fbd9001592fb1fa1f3f49e122e82d41c3c2d827
Yung.Energy <morskojroma@gmail.com>
2022-08-04 19:09:41 +0300

    task1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите числоN : ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    int max = m,
+        min = n;
+    if (max < min)
+    {
+        max = n;
+        min = m;
+    }
+    if (min == max)
+        return min;
+    else
+    {
+        Console.Write(NaturalNumber(max - 1, min) + ", ");
+        return max;
+    }
+}
